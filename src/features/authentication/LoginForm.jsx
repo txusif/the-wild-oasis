91964d0fa643219e5ab859a6a3d8db3a6a27b3ab
Txusif <txusif@gmail.com>
2024-10-21 12:03:36 +0530

    add: test credentials

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -8,8 +8,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("test@example.com");
+  const [password, setPassword] = useState("12345678");
   const { isLoading: isLogging, login } = useLogin();
 
   function handleSubmit(e) {
